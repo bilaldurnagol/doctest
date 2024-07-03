73afd1457cc350ffed3f4920ed105f870aaa5402
Bilal Durnagol <bilaldurnagol@gmail.com>
2024-07-04 00:13:34 +0300

    test

--- Sources/doctest/doctest.swift
+++ Sources/doctest/doctest.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+/// Bu class test içindir.
+public class DocTest {
+    
+    
+    /// Bu fonksiyon sdk'yi kurmayı sağlar
+    /// - Parameter userId: Kullanıcının uniq user idsidir
+    public func start(with userId: Int) {
+        
+    }
+}
---
+++ build-docc.sh
@@ -0,0 +1,13 @@
+##!/bin/sh
+
+xcrun xcodebuild docbuild \
+    -scheme doctest \
+    -destination 'generic/platform=iOS Simulator' \
+    -derivedDataPath "$PWD/.derivedData"
+
+xcrun docc process-archive transform-for-static-hosting \
+    "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/doctest.doccarchive" \
+    --output-path ".docs" \
+    --hosting-base-path "" # add your repo name later
+
+echo '<script>window.location.href += "/documentation/doctest"</script>' > .docs/index.html
